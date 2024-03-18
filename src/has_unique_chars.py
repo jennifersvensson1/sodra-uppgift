ea3c42f54dba5bc6a98b70f9598405c428095d0c
jennifersvensson1 <jennifer.svenssn@gmail.com>
2024-03-18 15:01:38 +0100

    Adds comments to python func

--- src/has_unique_chars.py
+++ src/has_unique_chars.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*- # Ensures proper handling of non-ASCII characters
 
 def has_unique_chars(string):
-    unique_chars = set() 
+    unique_chars = set() # Collection with only unique values
 
+    # Iterates through each character in the string and checks for duplicates
     for char in string:
         if char in unique_chars:
             return False 
         unique_chars.add(char)
     return True
 
+# Example usage of the function with strings containing unique and duplicate characters
 print(has_unique_chars("Södra"))
 print(has_unique_chars("Södra Skogsägarna"))
 print(has_unique_chars("Systemutvecklare"))
